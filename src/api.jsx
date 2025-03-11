a4d758ff6de5ebe0b0f1444ed2ea7818be73c6c2
MurugananthamB <murugananthamanand01212@gmail.com>
2025-03-11 12:04:03 +0530

    added

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/";
+const API_URL = "http://192.168.103.158:5000/";
+
+// http://localhost:5000/
   
   // "https://eyecamp-backend.onrender.com";
 
